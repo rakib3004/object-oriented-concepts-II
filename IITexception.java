8b1df0cd518a105f841bea0bf752f9d04e033ffa
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-14 14:31:30 +0600

    Add files via upload

--- IITexception.java
+++ IITexception.java
@@ -4,18 +4,44 @@ import javax.swing.*;
 
 public class IITexception {
     public static void main(String[] args) {
-String a = "BSSE1129";
-       /* System.out.println(a);
-        a= a.toLowerCase();
-        System.out.println(a);*/
-            String string =   JOptionPane.showInputDialog("Enter Your Roll Number : ");
-boolean bool = string.contains("BSSE");
-if(bool){
-    JOptionPane.showMessageDialog(null,"Your roll no is : "+string);
-    JOptionPane.showMessageDialog(null,"Your email  is : "+string.toLowerCase()+"@iit.du.ac.bd");
 
+
+        try
+        {
+            String string =   JOptionPane.showInputDialog("Enter Your Roll Number : ");
+            boolean bool = string.contains("BSSE");
+            if(bool){
+                JOptionPane.showMessageDialog(null,"Your roll no is : "+string);
+                JOptionPane.showMessageDialog(null,"Your email  is : "+string.toLowerCase()+"@iit.du.ac.bd");
+
+            }
+            throw new MyException("Please Enter BSSE format");
+        }
+        catch (MyException ex)
+        {
+          
+            System.out.println(ex.getMessage());
+        }
+
+
+
+
+    }
+}
+class MyException extends Exception
+{
+    public MyException(String string)
+    {
+        super(string);
+    }
 }
 
+// A Class that uses above MyException
+ class Main
+{
+    // Driver Program
+    public static void main(String args[])
+    {
 
     }
 }
