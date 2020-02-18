87838780254535928620efe8086272b527e1f6d6
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-18 08:23:29 +0600

    Add files via upload

--- IITexception.java
+++ IITexception.java
@@ -9,18 +9,27 @@ public class IITexception {
         try
         {
             String string =   JOptionPane.showInputDialog("Enter Your Roll Number : ");
-            boolean bool = string.contains("BSSE");
-            if(bool){
+            String check = string.substring(0,4);
+
+            boolean bool = check.equals("BSSE");
+            boolean bool1=bool;
+            for(int i=4;i<8;i++){
+
+                bool = (string.charAt(i)<='9'&& string.charAt(i)>='0');
+              bool1 = bool1 & bool;
+            }
+            if(bool1){
                 JOptionPane.showMessageDialog(null,"Batch No : "+string.substring(4,6)+"\n"
                         +"Roll No : "+ string.substring(6,8)+"\n"+"Your email  is : "+string.toLowerCase()+"@iit.du.ac.bd");
 
             }
-            throw new MyException("Please Enter BSSE format");
+            else
+            throw new MyException("Please Enter roll at BSSE format && put right roll no .");
         }
         catch (MyException ex)
         {
+JOptionPane.showMessageDialog(null,ex.getMessage());
 
-            System.out.println(ex.getMessage());
         }
 
 
@@ -35,4 +44,3 @@ class MyException extends Exception
         super(string);
     }
 }
-
