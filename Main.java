cbefe64a0a79e7c23ac0ed1ac2acc181d913ef12
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-18 09:37:17 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package NewPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Circle c1 = new Circle(5);   c1.setRadius(-5);
+            Circle c2 = new Circle(0);
+        }
+        catch (InvalidRadiusException ex) {
+            System.out.println("Invalid Radius: " + ex.getRadius());
+        }
+
+        System.out.println("Number of objects created: " +
+                Circle.getNumberOfObjects());
+    }
+}
