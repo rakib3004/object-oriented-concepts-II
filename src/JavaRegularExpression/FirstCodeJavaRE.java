7e473b8d092dc8df95e23ba35401f4eb7d40edea
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-02 23:02:03 +0600

    rename class and new string and pattern

--- src/JavaRegularExpression/FirstCodeJavaRE.java
+++ src/JavaRegularExpression/FirstCodeJavaRE.java
@@ -3,14 +3,14 @@ package src.JavaRegularExpression;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class JavaRE {
+public class FirstCodeJavaRE {
 
     public static void main(String[] args) {
-        String statement = new String("Institute of Information Technology");
-        String  patternString =  "I{1,2}";
+        String statement = "Welcome to BSSE 8th Batch";
+        String  patternString =  "S{2,8}";
         Pattern pattern = Pattern.compile(patternString);
 
         Matcher matcher = pattern.matcher(patternString);
-        System.out.println(matcher.find()?"Well Done" : "We are failed");
+        System.out.println(matcher.find()? "Well Done" : "We are failed");
     }
 }
