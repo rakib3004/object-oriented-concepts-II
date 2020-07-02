6bf0f283734132b4eb16bf58245189c9563dc6a2
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-02 23:48:14 +0600

    start to set string and set pattern and matcher

--- src/JavaRegularExpression/MatchesFindDifferent.java
+++ src/JavaRegularExpression/MatchesFindDifferent.java
@@ -1,4 +1,18 @@
 package src.JavaRegularExpression;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MatchesFindDifferent {
+
+    public static void main(String[] args) {
+        String matchString = "university of dhaka";
+        String findString = "notre dame college";
+        String matchPattern="[a-z ]+";
+        Pattern pattern1 = Pattern.compile(matchPattern);
+        Pattern pattern2 = Pattern.compile(findString);
+
+        Matcher matcher = pattern1.matcher(matchString);
+
+    }
 }
