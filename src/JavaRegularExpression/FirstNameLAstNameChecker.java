c86d5407419184c8c0e8185119f4415de6d160d5
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-04 06:04:39 +1200

    create FirstNameLAstNameChecker.java class and add two pattern of first name and last name pattern

---
+++ src/JavaRegularExpression/FirstNameLAstNameChecker.java
@@ -0,0 +1,9 @@
+package src.JavaRegularExpression;
+
+public class FirstNameLAstNameChecker {
+    public static void main(String[] args) {
+
+        String firstNamePattern ="[A-Z][a-z]+";
+        String lastNamePattern = "[A-Z]+([ '-][A-Z][a-z]*)";
+    }
+}
