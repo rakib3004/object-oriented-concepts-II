0a2cd69dfc4a0b6b4ffffa3ad8d8b8672fbb192e
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-03 10:38:23 +0600

    add new code in MatchesFindDifferent.java

--- src/JavaRegularExpression/MatchesFindDifferent.java
+++ src/JavaRegularExpression/MatchesFindDifferent.java
@@ -8,21 +8,22 @@ import java.util.regex.Pattern;
 public class MatchesFindDifferent {
 
     public static void main(String[] args) {
-        String matchString = "university of dhaka";
+        String matchString = "    ";
         String findString1 = "notre dame college";
-        String findString2 = "iitDU";
+        String findString2 = "@#$%%#";
         String findString3 = "iitdu@.ac.bd";
+        String findString4 = "-----===aaa==-------";
 
         String matchPattern="[a-z ]+";
-        String findPattern = "^[a-z]$";
+        String findPattern = "[^a-z]$";
         Pattern patternMatch = Pattern.compile(matchPattern);
         Pattern patternFind = Pattern.compile(findPattern);
 
         Matcher matcherMatch = patternMatch.matcher(matchString);
-        Matcher matcherFind = patternFind.matcher(findString1);
+        Matcher matcherFind = patternFind.matcher(findString2);
 
-        System.out.println("Result 1 : "+matcherMatch.matches());
-        System.out.println("Result 2 : "+matcherFind.find());
+        System.out.println("Matching Result : "+matcherMatch.matches());
+        System.out.println("Fining Result : "+matcherFind.find());
 
     }
 }
