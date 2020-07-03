a92b6edc071b31c6f30565ca09b8661ee996d376
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-04 02:12:35 +1200

    create PasswordCheckerJavaRE.java class to check any phone number is right or wrong

---
+++ src/JavaRegularExpression/PasswordCheckerJavaRE.java
@@ -0,0 +1,23 @@
+package src.JavaRegularExpression;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordCheckerJavaRE {
+    public static void main(String[] args) {
+        String passwordPattern = "[A-Z]+[a-z]+[0-9]+";
+        String passwordString ="Rakib1129";
+       /* Scanner scanner = new Scanner(System.in);
+        passwordPattern = scanner.nextLine();*/
+
+        Pattern pattern = Pattern.compile(passwordPattern);
+        Matcher matcher = pattern.matcher(passwordString);
+        boolean bool = matcher.find();
+        boolean aBool;
+        aBool = !bool;
+        String boolString = Boolean.toString(bool);
+        boolString = boolString.toUpperCase();
+        System.out.println(boolString);
+    }
+}
