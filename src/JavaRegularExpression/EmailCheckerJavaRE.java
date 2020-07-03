84f8ab9370adc1c5aaad3293736c514a8ee14d62
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-03 12:52:15 +1200

    create EmailCheckerJavaRE.java class and add code

---
+++ src/JavaRegularExpression/EmailCheckerJavaRE.java
@@ -0,0 +1,22 @@
+package src.JavaRegularExpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailCheckerJavaRE {
+
+    public static void main(String[] args) {
+
+        String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+*&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}";
+        String emailName = "bsse1129@iit.du.ac.bd";
+
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(emailName);
+       boolean bool = matcher.find();
+       boolean aBool = !bool;
+       String boolString = Boolean.toString(bool);
+       boolString = boolString.toUpperCase();
+        System.out.println(boolString);
+
+    }
+}
