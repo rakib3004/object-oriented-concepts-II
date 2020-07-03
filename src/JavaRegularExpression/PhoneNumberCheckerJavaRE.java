33a1826a88b4cb4c676d8119f2ef6bc1714cceb0
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-04 01:54:22 +1200

    create PhoneNumberCheckerJavaRE class to check any phone number is right or wrong

---
+++ src/JavaRegularExpression/PhoneNumberCheckerJavaRE.java
@@ -0,0 +1,23 @@
+package src.JavaRegularExpression;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberCheckerJavaRE {
+    public static void main(String[] args) {
+        String phoneNumberPattern = "01[3-9][0-9]{8}";
+        String phoneNumberExample = "01911803004";
+
+        Scanner scanner = new Scanner(System.in);
+        phoneNumberExample = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(phoneNumberPattern);
+        Matcher matcher = pattern.matcher(phoneNumberExample);
+        boolean bool = matcher.find();
+        boolean aBool = !bool;
+        String boolString = Boolean.toString(bool);
+        boolString = boolString.toUpperCase();
+        System.out.println(boolString);
+    }
+}
