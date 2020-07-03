84857ed533c17b30382aa11295a61d61e0f5a702
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-03 13:35:22 +1200

    add scanner in EmailCheckerJavaRE.java

--- src/JavaRegularExpression/EmailCheckerJavaRE.java
+++ src/JavaRegularExpression/EmailCheckerJavaRE.java
@@ -1,5 +1,6 @@
 package src.JavaRegularExpression;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +10,10 @@ public class EmailCheckerJavaRE {
 
         String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+*&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}";
         String emailName = "bsse1129@iit.du.ac.bd";
+
+        Scanner scanner = new Scanner(System.in);
+        emailName = scanner.nextLine();
+
 
         Pattern pattern = Pattern.compile(emailPattern);
         Matcher matcher = pattern.matcher(emailName);
