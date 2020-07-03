202066c79ed73a8b2869155cbb68fa8aba27c690
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-04 06:15:25 +1200

    ADD code in FirstNameLAstNameChecker.java class

--- src/JavaRegularExpression/FirstNameLAstNameChecker.java
+++ src/JavaRegularExpression/FirstNameLAstNameChecker.java
@@ -1,9 +1,24 @@
 package src.JavaRegularExpression;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FirstNameLAstNameChecker {
     public static void main(String[] args) {
 
         String firstNamePattern ="[A-Z][a-z]+";
-        String lastNamePattern = "[A-Z]+([ '-][A-Z][a-z]*)";
+        String lastNamePattern = "[A-Z]+([ '-][A-Z])*[a-z]*";
+
+        String userFirstName ="Rakib";
+        String userLastName = "Trofder";
+
+        Pattern patternFirstName = Pattern.compile(firstNamePattern);
+        Pattern patternLastName = Pattern.compile(lastNamePattern);
+
+        Matcher matcherFirstName = patternFirstName.matcher(userFirstName);
+        Matcher matcherLastName = patternLastName.matcher(userLastName);
+
+        System.out.println(matcherFirstName.matches()? "Your first name is correct." :"Your first name is wrong!!!");
+        System.out.println(matcherLastName.matches() ?"Your last name is correct." : "Your last name is wrong!!!" );
     }
 }
