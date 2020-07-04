3b9288c5a8e215d82d2c0db0311c817a93f5fdb5
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-04 18:28:02 +1200

    create MatrixChoiceMultlipication.java class

---
+++ src/CombinatorialOptimization/MatrixChoiceMultlipication.java
@@ -0,0 +1,25 @@
+package src.CombinatorialOptimization;
+
+public class MatrixChoiceMultlipication {
+    public static void main(String[] args) {
+int [] array = new int[]{30,35,15,5,10,20,25};
+int matrixNumber = 6;
+  int multiplicationCost =  matrixCalculation(array,1,matrixNumber);
+        System.out.println("Total cost for matrix multiplication is : "+multiplicationCost);
+
+    }
+    public static int matrixCalculation(int[] array, int i, int j){
+      if(i==j){
+          return  0;
+      }
+      int k;
+        int min=555555555;
+      for(k=i;k<j;k++){
+
+          int count = matrixCalculation(array,i,k)+matrixCalculation(array,k+1,j)+(array[i-1]*array[k]*array[j]);
+          min = Math.min(count,min);
+      }
+
+        return  min;
+    }
+}
