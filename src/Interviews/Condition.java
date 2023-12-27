4c101096457dc46f6b51bfb520e420b90f7e07f0
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-12-27 14:28:57 +0600

    code of steamstech interview

---
+++ src/Interviews/Condition.java
@@ -0,0 +1,11 @@
+package src.Interviews;
+
+public class Condition {
+    public static void main(String[] args) {
+        boolean status = false ;
+        for(int i=0; status; i++)
+        {
+            System.out.println("Hello Javatpoint");
+        }
+    }
+}
